0540d0eee9a0576d9e47a314bce531a9cc9aa8ef
shashikanth150 <156493234+shashikanth150@users.noreply.github.com>
2024-02-29 06:09:14 +0530

    URL Changed

--- src/webparts/reactCrud/ReactCrudWebPart.ts
+++ src/webparts/reactCrud/ReactCrudWebPart.ts
@@ -1,25 +1,24 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
 
-import * as strings from 'ReactCrudWebPartStrings';
-import ReactCrud from './components/ReactCrud';
-import { IReactCrudProps } from './components/IReactCrudProps';
+import * as strings from "ReactCrudWebPartStrings";
+import ReactCrud from "./components/ReactCrud";
+import { IReactCrudProps } from "./components/IReactCrudProps";
 //import createList from './Services/Services';
 export interface IReactCrudWebPartProps {
   description: string;
 }
 
 export default class ReactCrudWebPart extends BaseClientSideWebPart<IReactCrudWebPartProps> {
-
   private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  private _environmentMessage: string = "";
 
   public render(): void {
     const element: React.ReactElement<IReactCrudProps> = React.createElement(
@@ -30,8 +29,8 @@ export default class ReactCrudWebPart extends BaseClientSideWebPart<IReactCrudWe
         environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
         userDisplayName: this.context.pageContext.user.displayName,
-        context:this.context,
-        siteUrl:this.context.pageContext.web.absoluteUrl
+        context: this.context,
+        siteUrl: this.context.pageContext.web.absoluteUrl,
       }
     );
 
@@ -39,7 +38,7 @@ export default class ReactCrudWebPart extends BaseClientSideWebPart<IReactCrudWe
   }
 
   protected async onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then(message => {
+    return this._getEnvironmentMessage().then((message) => {
       this._environmentMessage = message;
     });
     // await this._getEnvironmentMessage().then(message => {
@@ -51,32 +50,42 @@ export default class ReactCrudWebPart extends BaseClientSideWebPart<IReactCrudWe
     // await createList(this.context.pageContext.web.absoluteUrl);
   }
 
-
-
   private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-        .then(context => {
-          let environmentMessage: string = '';
+    if (!!this.context.sdks.microsoftTeams) {
+      // running in Teams, office.com or Outlook
+      return this.context.sdks.microsoftTeams.teamsJs.app
+        .getContext()
+        .then((context) => {
+          let environmentMessage: string = "";
           switch (context.app.host.name) {
-            case 'Office': // running in Office
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
+            case "Office": // running in Office
+              environmentMessage = this.context.isServedFromLocalhost
+                ? strings.AppLocalEnvironmentOffice
+                : strings.AppOfficeEnvironment;
               break;
-            case 'Outlook': // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
+            case "Outlook": // running in Outlook
+              environmentMessage = this.context.isServedFromLocalhost
+                ? strings.AppLocalEnvironmentOutlook
+                : strings.AppOutlookEnvironment;
               break;
-            case 'Teams': // running in Teams
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+            case "Teams": // running in Teams
+              environmentMessage = this.context.isServedFromLocalhost
+                ? strings.AppLocalEnvironmentTeams
+                : strings.AppTeamsTabEnvironment;
               break;
             default:
-              throw new Error('Unknown host');
+              throw new Error("Unknown host");
           }
 
           return environmentMessage;
         });
     }
 
-    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
+    return Promise.resolve(
+      this.context.isServedFromLocalhost
+        ? strings.AppLocalEnvironmentSharePoint
+        : strings.AppSharePointEnvironment
+    );
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -85,16 +94,19 @@ export default class ReactCrudWebPart extends BaseClientSideWebPart<IReactCrudWe
     }
 
     this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
+    const { semanticColors } = currentTheme;
 
     if (semanticColors) {
-      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-      this.domElement.style.setProperty('--link', semanticColors.link || null);
-      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
+      this.domElement.style.setProperty(
+        "--bodyText",
+        semanticColors.bodyText || null
+      );
+      this.domElement.style.setProperty("--link", semanticColors.link || null);
+      this.domElement.style.setProperty(
+        "--linkHovered",
+        semanticColors.linkHovered || null
+      );
     }
-
   }
 
   protected onDispose(): void {
@@ -102,7 +114,7 @@ export default class ReactCrudWebPart extends BaseClientSideWebPart<IReactCrudWe
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -110,20 +122,20 @@ export default class ReactCrudWebPart extends BaseClientSideWebPart<IReactCrudWe
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
